fcbf518800b90c24a9949493873a762fa83baf74
Alexander Kuzmenko <alex@stablex.ru>
2013-01-06 01:56:47 +0400

    InputText added.

---
+++ ru/stablex/ui/widgets/InputText.hx
@@ -0,0 +1,32 @@
+package ru.stablex.ui.widgets;
+
+#if html5
+import nme.events.Event;
+#end
+
+
+
+/**
+*   classname:  InputText
+*
+* Text with type = nme.text.TextFieldType.INPUT
+*/
+class InputText extends Text{
+
+    /**
+    * Constructor
+    *
+    */
+    public function new () : Void {
+        super();
+
+        #if !html5
+            this.label.type = nme.text.TextFieldType.INPUT;
+        #else
+            //due to strange bug we need this hack
+            this.addEventListener(Event.ADDED_TO_STAGE, function(e:Event){
+                this.label.type = nme.text.TextFieldType.INPUT;
+            });
+        #end
+    }//function new()
+}//class InputText
