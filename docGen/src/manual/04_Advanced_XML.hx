020f7017bfc05dcf8aa399636b7a8bc46b4b82e3
Alexander Kuzmenko <alex@stablex.ru>
2013-01-11 16:57:05 +0400

    04_Advance_XML manual update

--- docGen/src/manual/04_Advanced_XML.hx
+++ docGen/src/manual/04_Advanced_XML.hx
@@ -194,6 +194,29 @@ And content of 'ui.xml':
 
 Now if we run this project and click the button, we'll see output: 'here is MyClass'.
 
+There are some frequently used classes preregistered in StablexUI. Here is the list:
+
+<pre>
+ ru.stablex.ui.widgets.Text;
+ ru.stablex.ui.widgets.InputText;
+ ru.stablex.ui.widgets.Panel;
+ ru.stablex.ui.widgets.Widget;
+ ru.stablex.ui.widgets.Bmp;
+ ru.stablex.ui.widgets.Button;
+ ru.stablex.ui.widgets.Box;
+ ru.stablex.ui.widgets.VBox;
+ ru.stablex.ui.widgets.HBox;
+ ru.stablex.ui.widgets.ViewStack;
+ ru.stablex.ui.widgets.Mask;
+ ru.stablex.ui.events.WidgetEvent;
+ ru.stablex.TweenSprite;
+ ru.stablex.ui.UIBuilder;
+ nme.Lib;
+ ru.stablex.Assets;
+ nme.events.Event;
+ nme.events.MouseEvent;
+</pre>
+
 <h3>#widgetId</h3>
 
 This placeholder is translated to haxe in following code:
@@ -208,7 +231,7 @@ So you can use it when you need to operate with properties inherited from <type>
 <?xml version="1.0" encoding="UTF-8"?>
 
 <Panel id="'root'" w="100" h="200" bgColor="0x002200">
-    <Button w="50" h="20" text="'hit'" on-click=" trace( #root.w ); "
+    <Button w="50" h="20" text="'hit'" on-click=" trace( #root.w ); " />
 </Panel>
 </xml>
 
@@ -221,7 +244,7 @@ You need this placeholder if you need to work with property, wich is not in list
 For example, `#Panel(root)` will be translated to:
 
 <haxe>
- ru.stablex.ui.UIBuilder.getAs('widgetId', ru.stablex.ui.widgets.Panel);
+ ru.stablex.ui.UIBuilder.getAs('root', ru.stablex.ui.widgets.Panel);
 </haxe>
 
 And following xml will output panel's color:
@@ -238,7 +261,73 @@ And following xml will output panel's color:
 /**
 @manual Xml arguments (`@someArg` placeholder)
 
-...IN PROGRESS...
-@someArg    - replaced with arguments.someArg. Arguments can be passed like
-this: <type>ru.stablex.ui.UIBuilder</type>.buildFn(xmlFile)({someArg:'some value', someArg2: 3.14});
+You can pass arguments to closures cretaed with <type>ru.stablex.ui.UIBuilder</type>.buildFn().
+It's an easy way to change data in xml on runtime.
+
+For example, let's implement alert box. Create `alert.xml` and place there following code:
+
+<xml>
+<?xml version="1.0" encoding="UTF-8"?>
+
+<VBox left="100" top="100" bgColor="0xbbbbbb" border="2" autoSize="true" padding="10" childPadding="5">
+    <Text text="@message" />
+    <Button w="50" h="20" text="'close'" bgColor="0x777777" border="1" on-click=" $this.wparent.free(); " />
+</VBox>
+</xml>
+
+That will be our alert box template. Now wee need main interface, from wich we will create alert boxes.
+Create `main.xml`:
+
+<xml>
+<?xml version="1.0" encoding="UTF-8"?>
+
+<HBox autoSize="true" padding="10" childPadding="5">
+    <InputText id="'input'" border="1" bgColor="0xFFFFFF" w="150" h="20" autoSize="false" text="'type any message here'"/>
+
+    <!-- Here we create on-click handler wich shows our alert box and pass input message as argument -->
+    <Button autoWidth="true" h="20" padding="5" text="'Show me the alert!'" bgColor="0xbbbbbb" border="1" on-click="
+        $Lib.current.addChild( $UIBuilder.buildFn('alert.xml')(
+            {
+                message : #InputText(input).text
+            }
+        ) );
+    "/>
+</HBox>
+</xml>
+
+And the last file we need - main class for project. Here is the code for Main.hx:
+
+<haxe>
+ package;
+
+import nme.display.StageAlign;
+import nme.display.StageScaleMode;
+import nme.Lib;
+import ru.stablex.ui.UIBuilder;
+
+/**
+* StablexUI example created in this manual: http://stablex.ru/ui/doc#manual/04_Advanced_XML.html
+* Section: Xml arguments (`@someArg` placeholder)
+*
+*/
+class Main extends nme.display.Sprite{
+    /**
+    * Enrty point
+    *
+    */
+    static public function main () : Void{
+        Lib.current.stage.align     = StageAlign.TOP_LEFT;
+        Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+
+        //initialize StablexUI
+        UIBuilder.init();
+
+        //Create our UI
+        Lib.current.addChild( UIBuilder.buildFn('main.xml')() );
+    }//function main()
+}//class Main
+</haxe>
+
+Build project or try <a href="/ui/demo/04_alert.swf" target="_blank">flash</a> version or <a href="/ui/demo/04_alert" target="_blank">html5</a>.
+Full sample code is included in samples/04_arguments on <a href="https://github.com/RealyUniqueName/StablexUI" target="_blank">GitHub</a>.
 */
