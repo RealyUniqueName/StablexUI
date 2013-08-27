6eca692ba7b6968d76a592065a1a8d96417e737c
Alexander Kuzmenko <alex@stablex.ru>
2013-08-27 15:48:40 +0400

    Lib abstraction

--- src/ru/stablex/backend/Lib.hx
+++ src/ru/stablex/backend/Lib.hx
@@ -1,3 +1,33 @@
 package ru.stablex.backend;
 
+#if (flash || openfl)
 typedef Lib = haxe.macro.MacroType<[ru.stablex.ui.ClassBuilder.getBackendClass("flash", "Lib")]>;
+
+#else
+typedef Lib = haxe.macro.MacroType<[ru.stablex.ui.ClassBuilder.getBackendClass("ru.stablex.backend", "Lib.LibRef")]>;
+
+
+/**
+* Reference for Lib implementation
+*
+*/
+class LibRef {
+
+    /**
+    * Root sprite on stage.
+    * `.stage` property of this sprite must not be null
+    */
+    static public var current : Sprite;
+
+
+    /**
+    * Constructor
+    *
+    */
+    public function new () : Void {
+        //code...
+    }//function new()
+
+}//class LibRef
+
+#end
