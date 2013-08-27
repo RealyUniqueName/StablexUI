c15c2fa76c46ce5488d5916afc1bdba22e808650
Alexander Kuzmenko <alex@stablex.ru>
2013-08-27 15:43:54 +0400

    utils.* abstraction

--- src/ru/stablex/backend/utils/ByteArray.hx
+++ src/ru/stablex/backend/utils/ByteArray.hx
@@ -1,3 +1,24 @@
 package ru.stablex.backend.utils;
 
+#if (flash || openfl)
 typedef ByteArray = haxe.macro.MacroType<[ru.stablex.ui.ClassBuilder.getBackendClass("flash", "utils.ByteArray")]>;
+
+#else
+typedef ByteArray = haxe.macro.MacroType<[ru.stablex.ui.ClassBuilder.getBackendClass("ru.stablex.backend", "utils.ByteArray.ByteArrayRef")]>;
+
+/**
+* Reference for ByteArray implementation
+*
+*/
+class ByteArrayRef {
+
+    /**
+    * Constructor
+    *
+    */
+    public function new () : Void {
+        //code...
+    }//function new()
+
+}//class ByteArrayRef
+#end
