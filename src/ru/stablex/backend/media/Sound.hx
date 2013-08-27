b753e789fb2f8fadd0d902e0ab73d3e527d6911c
Alexander Kuzmenko <alex@stablex.ru>
2013-08-27 15:42:15 +0400

    media.* abstraction

--- src/ru/stablex/backend/media/Sound.hx
+++ src/ru/stablex/backend/media/Sound.hx
@@ -1,3 +1,25 @@
 package ru.stablex.backend.media;
 
+#if (flash || openfl)
 typedef Sound = haxe.macro.MacroType<[ru.stablex.ui.ClassBuilder.getBackendClass("flash", "media.Sound")]>;
+
+#else
+typedef Sound = haxe.macro.MacroType<[ru.stablex.ui.ClassBuilder.getBackendClass("ru.stablex.backend", "media.Sound.SoundRef")]>;
+
+/**
+* Reference for Sound implementation
+*
+*/
+class SoundRef {
+
+    /**
+    * Constructor
+    *
+    */
+    public function new () : Void {
+        //code...
+    }//function new()
+
+}//class SoundRef
+
+#end
