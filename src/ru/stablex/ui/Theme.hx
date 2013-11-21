45557611336dffc68b0ede79b245bcf95f09e842
Alexander Kuzmenko <alex@stablex.ru>
2013-11-21 19:50:12 +0400

    theme bitmaps embedding

--- src/ru/stablex/ui/Theme.hx
+++ src/ru/stablex/ui/Theme.hx
@@ -1,5 +1,11 @@
 package ru.stablex.ui;
 
+/**
+* :INFO:
+* Special thanks to Dima (profelis) Granetchi for resource embedding code
+* https://github.com/profelis
+*/
+
 using StringTools;
 
 #if macro
@@ -7,6 +13,11 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 import sys.FileSystem;
 import sys.io.File;
+
+enum AssetType {
+    AImage;
+    ASound;
+}
 
 #else
 
@@ -23,17 +34,19 @@ class Theme {
     /** prefix for resource names */
     static public inline var RESOURCE_PREFIX = 'theme-assets:';
 
+#if !macro
     /** _bmpMeta */
     static public var bmpMeta : Map<String,{width:Int,height:Int}> = new Map();
     /** cache of bitmaps retreived from assets */
     static private var _bmpCache : Map<String,BitmapData> = new Map();
 
-#if macro
+#else
     /** match file name in full file path */
     static private var _erFile : EReg = ~/([^\\\/]+)$/;
     /** check if file is image */
-    static private var _erImg : EReg = ~/\.(jpg|jpeg|png)$/i;
-
+    static private var _erImg : EReg = ~/^(jpg|jpeg|png)$/i;
+    /** non alphanumeric characters */
+    static private var _erNonAlphaNum : EReg = ~/[^0-9a-zA-Z]/g;
 
     /**
     * Get directory of a file for current context
@@ -43,6 +56,42 @@ class Theme {
         var file : String = Context.getPosInfos(Context.currentPos()).file;
         return Theme._erFile.replace(file, '');
     }//function _dir()
+
+
+    /**
+    * Get meta name for asset
+    *
+    */
+    static private inline function _getMetaName(type:AssetType) {
+            return switch (type) {
+                    case AImage: ":bitmap";
+                    case ASound: ":sound";
+            }
+    }//function _getMetaName()
+
+
+    /**
+    * Get base type for asset
+    *
+    */
+    static private inline function _getKind(type:AssetType) {
+            return switch (type) {
+                case AImage: TDClass( { pack : ["flash", "display"], name : "BitmapData", params :[] } );
+                case ASound: TDClass( { pack : ["flash", "media"], name : "Sound", params :[] } );
+            }
+    }//function _getKind()
+
+
+    /**
+    * Get prefix for asset class
+    *
+    */
+    static private inline function _getPrefix(type:AssetType) {
+            return switch (type) {
+                case AImage: "Bitmap_";
+                case ASound: "Sound_";
+            }
+    }//functionget _getPrefix()
 
 #end
 
@@ -61,10 +110,17 @@ class Theme {
     * Path should be relative to calling file.
     *
     */
-    macro static public function addAssets (path:String) : Expr {
-        if( Context.defined('display') ) return macro {};
+    macro static public function addAssets (path:String) : Array<Field> {
+        if( Context.defined('display') ) return null;
 
-        var code : Array<Expr> = [];
+        var pos = Context.currentPos();
+        var fields : Array<Field> = Context.getBuildFields();
+
+        //package for assets classes
+        var pack : Array<String> = Context.getLocalClass().get().pack.copy();
+        pack.push('assets');
+
+        var fnBody : Array<Expr> = [];
 
         path = (
             path.charAt(path.length - 1) == '/' || path.charAt(path.length - 1) == '\\'
@@ -74,25 +130,55 @@ class Theme {
         var dir : String = Theme._dir().replace('\\', '/') + path;
 
         for(fname in FileSystem.readDirectory(dir)){
-            fname = dir + fname;
-
+            var name     : String = _erNonAlphaNum.replace(fname, '_');
+            var fullpath : String = Context.resolvePath(dir + fname);
+// trace(fullpath);
             //skip directories for now
-            if( FileSystem.isDirectory(dir + fname) ) continue;
+            if( FileSystem.isDirectory(fullpath) ) continue;
 
-            Context.addResource(RESOURCE_PREFIX + fname, File.getBytes(fname));
-            //if this is image, store meta data
-            if( Theme._erImg.match(dir + fname) ){
-                var ext : String = Theme._erImg.matched(1);
-                switch(ext.toLowerCase()){
-                    case 'jpg'|'jpeg':
-                    case 'png':
-                    case _: throw 'Unknown image format: ' + fname;
-                }
-                // code.push(macro
+            //asset type
+            var ext : String = fullpath.split('.').pop();
+            if( !_erImg.match(ext) ){
+                //do nothing with other asset types for now
+                continue;
             }
+
+            var type : AssetType = AImage;
+
+            var cls : TypeDefinition  = {
+                    pos      : pos,
+                    fields   : [],
+                    params   : [],
+                    pack     : pack,
+                    name     : Theme._getPrefix(type) + name,
+                    meta     : [{name:Theme._getMetaName(type), params:[macro $v{fullpath}], pos:pos}],
+                    isExtern : false,
+                    kind     : Theme._getKind(type),
+            };
+
+            Context.defineType(cls);
+
+            var src : String = path + fname;
+            var e   : Expr = Context.parse('new ' + cls.pack.join('.') + '.' + cls.name + '(0, 0)', pos);
+            fnBody.push(macro if( path == $v{src} ) return $e);
         }
 
-        return macro {};
+        //create method for retrieving bitmaps
+        fnBody.push(macro return null);
+        fields.push({
+            pos  : pos,
+            name : 'getBitmapData',
+            meta : null,
+            kind : FFun({
+                ret    : TPath({pack:['flash', 'display'], name:'BitmapData', params:[]}),
+                params : [],
+                expr   : {expr:EBlock(fnBody), pos:pos},
+                args   : [{name:'path', opt:false, type:TPath({pack:[], name:'String', params:[]})}]
+            }),
+            access : [AStatic, APublic]
+        });
+
+        return fields;
     }//function addAssets()
 
 #if !macro
--- themes/ru/stablex/themes/test/Main.hx
+++ themes/ru/stablex/themes/test/Main.hx
@@ -1,13 +1,20 @@
 package ru.stablex.themes.test;
 
+import flash.Lib;
+import flash.text.Font;
 import ru.stablex.ui.Theme;
 
+import flash.text.TextField;
+import flash.text.TextFormat;
+
+@:font('../../themes/ru/stablex/themes/test/assets/kata.ttf') class MyFont extends Font{}
 
 
 /**
 * Entry
 *
 */
+@:build(ru.stablex.ui.Theme.addAssets('assets'))
 class Main extends Theme {
 
     /**
@@ -15,7 +22,26 @@ class Main extends Theme {
     *
     */
     static public function main () : Void {
-        Theme.addResources('assets');
+        // Theme.addAssets('assets');
+
+        var bmp = Main.getBitmapData('assets/bg.png');
+
+        Lib.current.addChild(new flash.display.Bitmap(bmp));
+
+        Font.registerFont (MyFont);
+
+        var tf = new TextField();
+        tf.embedFonts = true;
+
+        var format = new TextFormat();
+        format.font = "Katamotz Ikasi";
+        format.size = 30;
+        tf.defaultTextFormat = format;
+        tf.width = 400;
+
+        Lib.current.addChild(tf);
+        tf.text = 'Hello, world!';
+
     }//function main()
 
 }//class Main
