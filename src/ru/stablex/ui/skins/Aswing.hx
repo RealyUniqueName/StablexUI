4fca44aa04ca5b495b3afcd66933ce448d459627
ProG4mr <prog4mr@gmail.com>
2013-12-14 18:46:13 +0000

    Aswing skin - Draw method improved, more variables exposed.

--- src/ru/stablex/ui/skins/Aswing.hx
+++ src/ru/stablex/ui/skins/Aswing.hx
@@ -58,10 +58,24 @@ class Aswing extends Skin {
 	public var color: Int = 0x009FEC;
 	
     private var colors : Array<Int>;
+	private var outerBorderColor : Array<Int>;
 	private var innerBorderColor : Array<Int>;
 	
+	
+	/** difference between the light and darkside of the button gradient. [0, 1]*/
+	public var gradFactor: Float = 0.35;
 	/** Used to create skins automaticaly */
 	public var mode:String = "";
+	/** Controls the ammount of border factor.*/
+	public var borderFactor:Float = 0.3;
+	/** Distance of the bevel effect. */
+	public var bevelAmmount:Float = 2;
+	/** Blur applied to the bevel effect */
+	public var bevelBlur:Float = 4;
+	/** Bevel alpha */
+	public var bevelAlpha: Float = 0.5;
+	/** Direction of the bevel effect */
+	public  var bevelAngle : Int = 90;
 	
 	
     /**
@@ -73,6 +87,7 @@ class Aswing extends Skin {
         this.rotation = Math.PI / 2;
 		corners = [0];
 		colors = [0xFFFFFF, 0x0];
+		outerBorderColor = [0xFFFFFF, 0x0];
 		innerBorderColor = [0xFFFFFF, 0x0];
     }
 
@@ -92,30 +107,34 @@ class Aswing extends Skin {
     */
     override public function draw (w:Widget) : Void 
 	{
-			
-		if (Std.is(w, Button))
-		{
-			drawButton(cast w);
-		}
-		// TODO - else..
+		this._adjustArrays();
+		var angle = rotation;
+		var shadownAngle = -bevelAngle;
+		var _colors:Array<Int> = colors.copy();
 		
-    }
-	
-	function drawButton(w:Button) 
-	{
+		if (Std.is(w, Button)) 
+		{
+			if (mode == 'hoovered')
+			{
+				_colors[0] = ColorUtils.brighten(_colors[0], 0.2);
+				_colors[1] = ColorUtils.brighten(_colors[1], 0.2);
+				innerBorderColor[0] = ColorUtils.brighten(innerBorderColor[0], 0.2);
+				innerBorderColor[1] = ColorUtils.brighten(innerBorderColor[1], 0.2);
+			} 
+			
+			if (mode == 'pressed')
+			{
+				angle *= -1;
+				shadownAngle *= -1;
+			}
+		}
 		
 		var width  : Float = w.w - this.paddingLeft - this.paddingRight;
         var height : Float = w.h - this.paddingTop - this.paddingBottom;
 		//if size is wrong, draw nothing
         if ( width <= 0 || height <= 0 ) return;
 		
-		var angle = rotation;
-		var shadownAngle = 225;
-		if (mode == 'pressed')
-		{
-			angle = 180;
-			shadownAngle = 45;
-		}
+		
 		
         var mx : Matrix = new Matrix();
         mx.createGradientBox(
@@ -125,75 +144,28 @@ class Aswing extends Skin {
             this.tx,
             this.ty
         );
-		var mxBase = mx.clone();
-		
-        this._adjustArrays();
-		var btnColor:Array<Int> = colors.copy();
-		
-		if (mode == 'hoovered')
-		{
-			btnColor[0] = ColorUtils.brighten(btnColor[0], 0.2);
-			btnColor[1] = ColorUtils.brighten(btnColor[1], 0.2);
-			innerBorderColor[0] = ColorUtils.brighten(innerBorderColor[0], 0.2);
-			innerBorderColor[1] = ColorUtils.brighten(innerBorderColor[1], 0.2);
-		} 
-		
-		
-		
-		// ===== BASE GRADIENT =====
-        w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL), btnColor, this.alphas, this.ratios, mxBase);
-		roundRect(w.graphics, this.paddingLeft, this.paddingTop, width, height, this.corners[0] / 2 );
+
         
 		// ===== OUTER BORDER =====
-		w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL),[innerBorderColor[1], innerBorderColor[1]], [1, 0.6], [0, 255], mx);
-		roundRect(w.graphics, this.paddingLeft, this.paddingTop, width, height, this.corners[0] / 2);
+		w.graphics.lineStyle(4, 0, 1, true);
+		w.graphics.lineGradientStyle((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL),[outerBorderColor[1], outerBorderColor[1]], [1, 1], ratios, mx);
+		w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL), [outerBorderColor[1], outerBorderColor[1]], [1, 1], ratios, mx);
+		w.graphics.drawRoundRect(this.paddingLeft, this.paddingTop, width, height, corners[0], corners[1]);
 		// ===== INNER BORDER =====
-		w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL),[innerBorderColor[0], innerBorderColor[0]], [1, 0.6], [0, 255], mx);
-		roundRect(w.graphics, this.paddingLeft + 1, this.paddingTop + 1, width - 2, height - 2, this.corners[0] / 2);
-		
+		w.graphics.lineStyle(2, 0, 1, true);
+		w.graphics.lineGradientStyle((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL),[innerBorderColor[0], innerBorderColor[1]], [1, 1], ratios, mx);
+		w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL), [innerBorderColor[0], innerBorderColor[1]], [1, 1], ratios, mx);
+		w.graphics.drawRoundRect(this.paddingLeft, this.paddingTop, width, height, corners[0], corners[1]);
 		// ===== INNER BUTTON =====
-		w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL), btnColor, this.alphas, this.ratios, mxBase);
-		roundRect(w.graphics, this.paddingLeft + 2, this.paddingTop + 2, width - 4, height - 4, this.corners[0] / 2);
-		
+		w.graphics.lineStyle(0, 0, 0, true);
+		w.graphics.beginGradientFill((this.type == 'linear' ? GradientType.LINEAR : GradientType.RADIAL), _colors, this.alphas, this.ratios, mx);
+		w.graphics.drawRoundRect(this.paddingLeft, this.paddingTop, width, height, corners[0], corners[1]);
 		w.graphics.endFill();
 		
+		// ===== APPLY BEVEL ===== 
+		w.filters = [new DropShadowFilter(bevelAmmount, shadownAngle, 0, bevelAlpha, bevelBlur, bevelBlur, 1, 3, true), new DropShadowFilter(bevelAmmount, -shadownAngle, 0xFFFFFF, bevelAlpha, bevelBlur, bevelBlur, 1, 3, true)];
 		
-		w.filters = [new DropShadowFilter(2,shadownAngle,0,.5,4,4,1,1,true), new DropShadowFilter(2,shadownAngle + 180,0xFFFFFF,.5,4,4,1,1,true)];
-	}
-	
-	/**
-	 * Draws a round rectangle.
-	 *
-	 * @param x the left top the rectangle bounds' x corrdinate.
-	 * @param y the left top the rectangle bounds' y corrdinate.
-	 * @param width the width of rectangle bounds.
-	 * @param height the height of rectangle bounds.
-	 * @param radius the top left corner's round radius.
-	 * @param trR (optional)the top right corner's round radius. (miss this param default to same as radius)
-	 * @param blR (optional)the bottom left corner's round radius. (miss this param default to same as radius)
-	 * @param brR (optional)the bottom right corner's round radius. (miss this param default to same as radius)
-	 */
-	public function roundRect(target:Graphics, x:Float, y:Float, width:Float, height:Float, radius:Float = 0, ?trR:Float = -1, ?blR:Float = -1, ?brR:Float = -1):Void {
-        var tlR:Float = radius;
-        if (trR == -1) trR = radius;
-        if (blR == -1) blR = radius;
-        if (brR == -1) brR = radius;
-		
-		//Bottom right
-		target.moveTo(x + blR, y + height);
-		target.lineTo(x + width - brR, y + height);
-		target.curveTo(x + width, y + height, x + width, y + height - blR);
-		//Top right
-		target.lineTo(x + width, y + trR);
-		target.curveTo(x + width, y, x + width - trR, y);
-		//Top left
-		target.lineTo(x + tlR, y);
-		target.curveTo(x, y, x, y + tlR);
-		//Bottom left
-		target.lineTo(x, y + height - blR);
-		target.curveTo(x, y + height, x + blR, y + height);
-	}
-	
+    }
 	
     /**
     * Set `.alphas` and `.ratios` according to `.colors` if they were not set manually
@@ -201,10 +173,12 @@ class Aswing extends Skin {
     */
     private inline function _adjustArrays () : Void {
 			
-		colors[0] = ColorUtils.brighten( Std.int((0xff << 24) | color), 0.35 / 2); // bright base color
-		colors[1] = ColorUtils.darken( Std.int((0xff << 24) | color), 0.35 / 2); // dark base color
-		innerBorderColor[0] = ColorUtils.brighten( Std.int((0xff << 24) | color), 0.35 / 2 + 0.15); // bright border color
-		innerBorderColor[1] = ColorUtils.darken( Std.int((0xff << 24) | color), 0.35 / 2 + 0.15); // dark border color; 
+		colors[0] = ColorUtils.brighten( Std.int((0xff << 24) | color), gradFactor / 2); // bright base color
+		colors[1] = ColorUtils.darken( Std.int((0xff << 24) | color), gradFactor / 2); // dark base color
+		innerBorderColor[0] = ColorUtils.brighten( Std.int((0xff << 24) | color), gradFactor / 2 + borderFactor); // bright border color
+		innerBorderColor[1] = ColorUtils.brighten( Std.int((0xff << 24) | color), gradFactor / 2 + borderFactor / 2); // dark border color; 
+		outerBorderColor[0] = ColorUtils.darken( Std.int((0xff << 24) | color), gradFactor / 2 + borderFactor); // bright border color
+		outerBorderColor[1] = ColorUtils.darken( Std.int((0xff << 24) | color), gradFactor / 2 + borderFactor / 2); // dark border color; 
 		
         if( this.alphas == null || this.alphas.length != this.colors.length ){
             this.alphas = [];
