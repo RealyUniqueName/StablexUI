5ce667d11a6e7b3bd56f8ee41e10c00aae5101fe
Liam Routt <liam@routt.net>
2014-02-10 12:17:49 +1100

    Fix for issue #35 - builds for iOS fail, unable to find defaults.xml

--- src/ru/stablex/ui/UIBuilder.hx
+++ src/ru/stablex/ui/UIBuilder.hx
@@ -125,7 +125,7 @@ class UIBuilder {
 
         //If provided with file for defaults, generate closures for applying defaults to widgets
         if( defaultsXmlFile != null ){
-            var root : Xml = Xml.parse( File.getContent(defaultsXmlFile) ).firstElement();
+            var root : Xml = Xml.parse( File.getContent(Context.resolvePath(defaultsXmlFile)) ).firstElement();
             for(widget in root.elements()){
                 code += '\nif( !ru.stablex.ui.UIBuilder.defaults.exists("' + widget.nodeName + '") ) ru.stablex.ui.UIBuilder.defaults.set("' + widget.nodeName + #if haxe3 '", new Map());' #else '", new Hash());' #end;
                 for(node in widget.elements()){
@@ -665,7 +665,7 @@ class UIBuilder {
     macro static public function buildFn (xmlFile:String) : Expr{
         UIBuilder._checkInit();
 
-        var element = Xml.parse( File.getContent(xmlFile) ).firstElement();
+        var element = Xml.parse( File.getContent(Context.resolvePath(xmlFile)) ).firstElement();
         var cls : String = UIBuilder._imports.get(element.nodeName);
 
         #if display
@@ -724,7 +724,7 @@ class UIBuilder {
 
         UIBuilder._checkInit();
 
-        var element = Xml.parse( File.getContent(xmlFile) ).firstElement();
+        var element = Xml.parse( File.getContent(Context.resolvePath(xmlFile)) ).firstElement();
 
         var code   : String = '';
         var erSkin : EReg = ~/^([a-z0-9_]+):([a-z0-9_]+)$/i;
